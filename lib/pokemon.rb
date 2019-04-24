f24041ea18ff9d113b71b683f514ef647f97322f
Eyal Toledano <eutait@gmail.com>
2019-04-24 10:04:09 -0400

    Passes the main Pokemon spec.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+  def initialize(id = nil, name = nil, type = nil, db)
+    @id = id
+    @db = db
+    @name = name
+    @type = type
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    insertion = db.prepare("INSERT INTO pokemon (name, type) VALUES (?, ?)")
+    insertion.execute(name,type)
+  end
+
+  def self.find(id, db)
+    query = db.execute("SELECT * FROM pokemon WHERE id = #{id};")
+    query = query.flatten
+    pokemon = self.new(query[0],query[1],query[2],db)
+    # binding.pry
+  end
+
 end
