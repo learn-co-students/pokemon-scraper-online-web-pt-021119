facbf4696185d6f4db8182276eee7f34c3881493
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-24 23:07:19 -0500

    Submit completed lab without bonus questions. Still working through bonus.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    search = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    pokemon_array = search[0]
+    test = Pokemon.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], db: db, hp: 60)
+  end
+
+
 end
