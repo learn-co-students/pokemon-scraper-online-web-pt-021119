f8b414d2498fd728c3e898d9a3b5f2014e569f43
David Spradling <jspradling83@yahoo.com>
2019-04-30 10:04:52 -0500

    made several changes to file
    worked on the first few examples and got them to pass

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def id
+  end
+
+  def name
+  end
+
+  def type
+  end
+
+  def db
+  end
+
+  def self.save(name, type, db)
+    db.execute( "INSERT INTO pokemon ( Name, Type ) VALUES (?, ? )", [name, type])
+    #self.type = "electric",
+    #self.db = @db
+  end
 end
